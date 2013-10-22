8635ea24c57c5d32c8719a13d58ef7db3bc38f8d
tzwm <wangjunzhe4444@gmail.com>
2013-10-22 21:08:57 +0800

    Initialize.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DriftingLibraryRor::Application.config.secret_key_base = 'a44c59326a2737af5f89542b4a0f0c70f12eec42c81c94de88cbad915d4d33d46fbffb0f68192d0f7ec2d4a2be8926ad24a281800475b522b6a147aa1880a2e8'
