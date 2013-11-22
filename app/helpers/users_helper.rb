7b0a5177655f25510cdfce18028261fd6bc56df8
cettco <zhangshiqi68@gmail.com>
2013-11-22 12:20:55 -0500

    register function finish

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+	def gravatar_for(id)
+		@current=User.find(id);
+    	gravatar_id = Digest::MD5::hexdigest(@current.email.downcase)
+    	gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    	image_tag(gravatar_url, alt: @current.name, class: "user_photo")
+  end
+  def name_for(id)
+  	@current=User.find(id);
+  	@current.name
+  end
 end
