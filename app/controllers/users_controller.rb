0dbee16749d03da652779cb516ffa3744c5cf1a2
cettco <zhangshiqi68@gmail.com>
2013-11-23 10:53:31 -0500

    user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,50 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
   def new
-  	@user = User.new
+    @user = User.new
   end
   def create
     @user = User.new(user_params)
     if @user.save
       # Handle a successful save.
+      sign_in @user
       flash[:success] = "Register successful"
       redirect_to @user
     else
       render 'new'
     end
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+     :password_confirmation)
+  end
+  def signed_in_user
+    unless signed_in?
+      flash[:notice] = "Please sign in."
+      redirect_to signin_url
     end
+  end
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
+  
 end
