0a0a981cc5fad6b51ff4b20c0f0742cfa6055350
tzwm <wangjunzhe4444@gmail.com>
2013-12-06 05:21:23 +0800

    Add five_random_books_image_tag in WelcomHelper.

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,14 @@
 module WelcomeHelper
+  def five_random_books_image_tag
+    ret = ""
+    5.times do
+      randomID = Random.rand(Book.count) + 1
+      book = Book.find(randomID)
+      imgUrl = book.image
+      title = book.title
+      ret += link_to(image_tag(imgUrl, alt: title, class: "img_cover"), book_path(book)) + "\n"
+    end
+    
+    return ret.html_safe
+  end
 end
